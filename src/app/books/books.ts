39492753eff1b460bb32a9ac634ec18e32a9beb3
Dn5299 <dn4998546@gmail.com>
2026-05-31 22:10:12 +0700

    first commit

--- src/app/books/books.ts
+++ src/app/books/books.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-books',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './books.html',
   styleUrl: './books.css',
+
 })
-export class Books {}
+export class Books {
+  books =[
+    {
+    stt: 1,
+    id: "A01",
+    title: "Dế phèn phiêu lưu ký",
+    author: "Tô Hoài",
+    }
+  ];
+}
